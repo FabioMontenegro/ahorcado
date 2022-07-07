c51349afdfb9c8dbfe9526ca2b197df45ecad3c4
Fabio Montenegro <fabiomontenegro1987@gmail.com>
2022-07-07 00:17:00 -0500

    proyecto actualizado, listo para probar

--- ahorcado.php
+++ ahorcado.php
@@ -3,9 +3,8 @@
         "submarino", "hamburguesa", "tequila", "ginebra", "pizza", "pelicula", "sixpack"
     ];
     define("intentos_maximos",5);
-    echo "Bienvenida al juego del ahorcado \n \n ";
-    
-    $palabra_seleccionada = $palabras(rand(0,7));
+    echo "Bienvenido al juego del ahorcado \n \n ";
+        $palabra_seleccionada = $palabras(rand(0,7));
     $longitud_palabra = strlen($palabra_seleccionada);
     $letras_descubiertas = str_pad("",$longitud_palabra,"_");
     $intentos = 0;
@@ -15,12 +14,20 @@
 
     $letras_jugador = readline(("Escribe una letra: "));
 
-    if(str_contains($palabra_seleccionada,$letras_jugador)){
+    if(str_contains($palabra_seleccionada, $letras_jugador)){
         //verificamos todas las veces que se repite esta letra para reemplazarla
-        while($posicion_letra = strpos()){
+        $offset =0;
+        while($posicion_letra = strpos($palabra_seleccionada, $letras_jugador, $offset) !==false){
+            $letras_descubiertas[$posicion_letra] = $letras_jugador;
+            $offset = $posicion_letra + 1;
 
         }
     }
+    else {
+        $intentos =1; 
+        echo "letra incorrecta. Te quedan " . (intentos_maximos - $intentos) . " intentos \n" ;
+    }
+    
 
 
 
